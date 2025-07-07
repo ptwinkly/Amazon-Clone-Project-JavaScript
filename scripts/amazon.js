8031c3652b431d40b1b4fe622b5f7449a87b6f83
WimonsineeYJ <161793199+ptwinkly@users.noreply.github.com>
2025-07-07 19:26:10 +0700

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -84,7 +84,14 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
-      //console.log(cart);
+      // Count Cart Quantity : Total items in the cart
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
     });
 });
