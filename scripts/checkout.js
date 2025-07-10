c0de6701d0f94987734a679ba7cb4f1c2a86c193
WimonsineeYJ <161793199+ptwinkly@users.noreply.github.com>
2025-07-10 21:51:34 +0700

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,6 @@
-import {renderOrderSummary} from './chekout/orderSummary.js'
+import {renderOrderSummary} from './chekout/orderSummary.js';
+
+import {renderPaymentSummary} from './chekout/paymentSummary.js';
 
 renderOrderSummary();
+renderPaymentSummary();
