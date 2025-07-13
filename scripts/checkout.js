4c031c5c52ddc03fc20ce90eb4780508acb0ff85
WimonsineeYJ <161793199+ptwinkly@users.noreply.github.com>
2025-07-13 22:32:37 +0700

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,8 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 
+import '../data/cart-oop.js'
+
 
 
 renderCheckoutHeader();
